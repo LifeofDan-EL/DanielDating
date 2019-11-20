5fe5cd062c566354bf832057ab30ec1710589a98
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-11-20 16:04:00 +0100

    And here it begins...

--- app/src/main/java/com/danielanomfueme/danieldating/LoginActivity.java
+++ app/src/main/java/com/danielanomfueme/danieldating/LoginActivity.java
@@ -2,17 +2,38 @@ package com.danielanomfueme.danieldating;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "LoginActivity";
+
+    private Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Log.d(TAG, "onCreate: Login Activity Started");
+        mLogin = findViewById(R.id.button_login);
+
+        mLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_login){
+            Log.d(TAG, "Onclick: Clicked, user logging in.");
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+
+        }
     }
 }
