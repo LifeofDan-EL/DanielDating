fc889e45d23e673f551a97c38d2c5708ecc23ff5
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-11-21 13:44:43 +0100

    And here it begins...

---
+++ app/src/main/java/com/danielanomfueme/danieldating/Util/Users.java
@@ -0,0 +1,34 @@
+package com.danielanomfueme.danieldating.Util;
+
+import android.net.Uri;
+
+import com.danielanomfueme.danieldating.R;
+import com.danielanomfueme.danieldating.models.User;
+
+public class Users {
+    public User[] USERS = {
+            Daniel, Angel, Curry, p4
+    };
+
+
+    /*
+            Men
+     */
+    public static final User Daniel = new User(Uri.parse("android.resource://com.danielanomfueme.danieldating/" + R.drawable.daniel).toString(),
+            "Daniel", "Male","Female", "Looking");
+
+    public static final User Curry = new User(Uri.parse("android.resource://com.danielanomfueme.danieldating./" + R.drawable.curry).toString(),
+            "Curry", "Male", "Female", "Looking");
+
+
+    /*
+            Females
+     */
+
+
+    public static final User Angel = new User(Uri.parse("android.resource://com.danielanomfueme.danieldating./" + R.drawable.angel).toString(),
+            "Angel", "Female","Male", "Looking");
+    public static final User p4 = new User(Uri.parse("android.resource://com.danielanomfueme.danieldating/" + R.drawable.p4).toString(),
+            "Phone", "Binary","Male and Female", "Need an Owner");
+
+}
